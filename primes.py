e79625fda31ec8b6d559eafa27ab606dc0aab5ca
RickSanchez88E <110410215+RickSanchez88E@users.noreply.github.com>
2023-09-29 08:32:51 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,6 +1,11 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
     list = []
+    if number_of_primesb == 0 or number_of_primes==1 or number_of_primes<0:
+        return []
+    else:
+        for i in range(2,number_of_primes):
+            if number_of_primes % i:
+                list.append(number_of_primes)
+            i += 1
     return list
